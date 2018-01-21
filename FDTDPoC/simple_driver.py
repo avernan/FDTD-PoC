3c03d0ed048f56448d45959dc994a48d50093d0f
Stefano Guazzotti <avernan@gmail.com>
2018-01-21 20:06:33 +0000

    Update `simple_driver.py` to use new features

--- FDTDPoC/simple_driver.py
+++ FDTDPoC/simple_driver.py
@@ -5,8 +5,24 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from matplotlib.colors import LogNorm
 import FDTDPoC.engine.solver as FDTD
+import FDTDPoC.engine.sources as sources
 
 g = FDTD.Grid(301,301)
+for i in range(5):
+    pos = numpy.random.random(2)*300
+    parms = 2*(numpy.random.random(4) - 0.5)
+    eff = 0.5
+    g.add_source(
+        sources.SourceDipole(
+            (int(pos[0]), int(pos[1])),
+            sources.PulseGaussian(
+                1 * (1 + eff*parms[0]),
+                300 * (1 + eff*parms[1]),
+                100 * (1 + eff*parms[2]),
+                1/40 * (1 + eff*parms[3])
+            )
+        )
+    )
 
 fig = plt.figure()
 ax = plt.axes(xlim=(-0.5,300.5), ylim=(-0.5,300.5))
@@ -20,20 +36,13 @@ text0 = ax.text(5,90, "f={:4d}".format(0))
 text0.set_color('r')
 text0.set_fontsize(20)
 
-mu = 300
-w = 1/40
-s = 100
-
-def source(i):
-    return numpy.math.exp(-(i - mu)**2/s**2) * numpy.math.cos(2*numpy.math.pi*w*i)
-
 def init():
     data = g.get_field(2)._data
     im.set_data(numpy.abs(data))
     return [im]
 
 def update(i):
-    g.step(i, (200,200,source(i)))
+    g.step(i)
     data = g.get_field(2)._data
     im.set_data(numpy.abs(data))
     text0.set_text("f={:4d}".format(i))
