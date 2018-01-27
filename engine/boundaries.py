26ecda1462d02d317d4a6ce474bbc27307177658
Stefano Guazzotti <avernan@gmail.com>
2018-01-27 11:58:03 +0000

    Move empty constructor to parent class

--- engine/boundaries.py
+++ engine/boundaries.py
@@ -1,14 +1,13 @@
 
 class Boundary(object):
-    pass
+    def __init__(self):
+        pass
+
 
 class PEC(Boundary):
     """
     Perfect Electric Conductor (PEC) boundary.
     Perfectly reflecting boundary obtained by forcing the terminating E node to zero
     """
-    def __init__(self):
-        pass
-
     def __call__(self, *args, **kwargs):
         return 0
