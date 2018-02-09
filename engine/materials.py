fa15d6a815ffa14da22188f43637df4b46848789
Stefano Guazzotti <avernan@gmail.com>
2018-02-09 15:10:07 +0000

    Change passive materials to be stored as slice instances

--- engine/materials.py
+++ engine/materials.py
@@ -1,9 +1,10 @@
 import numpy
 
 class PassiveMaterial(object):
-    def __init__(self, bleft, tright, n):
-        self.w = tright[0] - bleft[0]
-        self.h = tright[1] - bleft[1]
+    def __init__(self, n, bleft=None, tright=None):
         self.n = n
-    def __call__(self):
-        return numpy.ones((self.w,self.h)) * self.n
+        self.region = (slice(bleft[0], tright[0]), slice(bleft[1], tright[1]))
+
+    def build(self):
+        return  self.n * numpy.ones((self.region[0][1] - self.region[0][0],
+                                     self.region[1][1] - self.region[1][0]))
