385f1785458176bb9cb81015888c79fa4b80b92b
Stefano Guazzotti <avernan@gmail.com>
2018-02-09 15:40:48 +0000

    Size of the returned array should be consistent with region slices

--- engine/materials.py
+++ engine/materials.py
@@ -3,8 +3,8 @@ import numpy
 class PassiveMaterial(object):
     def __init__(self, n, bleft, tright):
         self.n = n
-        self.region = (slice(bleft[0], tright[0]), slice(bleft[1], tright[1]))
+        self.region = (slice(bleft[0], tright[0]+1), slice(bleft[1], tright[1]+1))
 
     def build(self):
-        return  self.n * numpy.ones((self.region[0][1] - self.region[0][0] + 1,
-                                     self.region[1][1] - self.region[1][0] + 1))
+        return  self.n**2 * numpy.ones((self.region[0].stop - self.region[0].start,
+                                     self.region[1].stop - self.region[1].start))
